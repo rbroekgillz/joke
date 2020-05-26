a8e903175b5c9b0191790e41b17a17fab22714a2
Ron Broek <r.broek@gillz.nl>
2020-05-26 12:18:38 +0200

    Add very funny joke

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+const page = `
+<html>
+  <head>
+  <title>Ron Broek</title>
+  </head>
+  <body>
+    <h1>Why do comedians love !false?</h1>
+    <p>It's funny because it's true.</p>
+  </body>
+</html>`;
+
+app.get('/', (req, res) => {
+  res.send(page);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on :${PORT}`);
+});
